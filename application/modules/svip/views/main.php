e45809bc739f04f92a25efcb58660bb0d77dd1c9
jymsy <jym20083617@163.com>
2016-10-17 22:40:34 +0800

    update

--- application/modules/svip/views/main.php
+++ application/modules/svip/views/main.php
@@ -4,10 +4,13 @@
     <meta charset="UTF-8">
     <title>Document</title>
     <link rel='stylesheet' type='text/css' href='<?php echo base_url("/static/base.css"); ?>'>
+    <link rel='stylesheet' type='text/css' href='<?php echo base_url("/static/base.min.css"); ?>'>
 </head>
 <body>
-<?php echo $content  ?>
+    <div class="warrp_v422 clearfix">
+    <?php echo $content  ?>
+    </div>
 <script src="http://lib.sinaapp.com/js/jquery/1.9.1/jquery-1.9.1.min.js"></script>
-<script src="<?php echo base_url("/static/dist/bundle.js"); ?>"></script>
+<script src="<?php echo base_url("/static/js/dist/app.js"); ?>"></script>
 </body>
 </html>
--- static/webpack.config.js
+++ static/webpack.config.js
@@ -2,22 +2,22 @@ var webpack = require('webpack');
 var uglifyJsPlugin = webpack.optimize.UglifyJsPlugin;
 
 module.exports = {
-    entry: './js/src/common.js',
+    entry: './js/src/index.js',
     output: {
         path: 'js/dist',
-        filename: 'app_[hash:10].js'
+        filename: 'app.js'
     },
     externals: {
         jquery: 'jQuery'
     },
-    plugins: [
-        new uglifyJsPlugin({
-            compress: {
-                warnings: false
-            },
-            output: {
-                comments: false
-            }
-        })
-    ]
+    // plugins: [
+    //     new uglifyJsPlugin({
+    //         compress: {
+    //             warnings: false
+    //         },
+    //         output: {
+    //             comments: false
+    //         }
+    //     })
+    // ]
 };
