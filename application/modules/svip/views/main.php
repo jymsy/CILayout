cd3a879c5cc431637ddf764bc8a39bfce89fcea9
jymsy <jym20083617@163.com>
2016-11-17 22:23:18 +0800

    u

--- application/modules/svip/views/main.php
+++ application/modules/svip/views/main.php
@@ -3,14 +3,16 @@
 <head>
     <meta charset="UTF-8">
     <title>Document</title>
-    <link rel='stylesheet' type='text/css' href='<?php echo base_url("/static/base.css"); ?>'>
-    <link rel='stylesheet' type='text/css' href='<?php echo base_url("/static/base.min.css"); ?>'>
+    <link rel='stylesheet' type='text/css' href='<?php echo base_url("/static/css/base.css"); ?>'>
+<!--    <link rel='stylesheet' type='text/css' href='--><?php //echo base_url("/static/base.min.css"); ?><!--'>-->
 </head>
 <body>
     <a href="#" onclick="clicked()">xxxxxxxxxx</a>
     <div class="warrp_v422 clearfix">
     <?php echo $content  ?>
+        <div class="leaf-loading"></div>
     </div>
+
 <script src="http://lib.sinaapp.com/js/jquery/1.9.1/jquery-1.9.1.min.js"></script>
 <script src="<?php echo base_url("/static/js/dist/vendors.js"); ?>"></script>
 <script src="<?php echo base_url("/static/js/dist/learningcenter.js"); ?>"></script>
